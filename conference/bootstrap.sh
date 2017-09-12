833ab4a922474b311dcef0df539fd2c0aba30510
Pig Monkey <pm@pig-monkey.com>
2017-09-11 17:49:23 -0700

    upgrade last

--- conference/bootstrap.sh
+++ conference/bootstrap.sh
@@ -13,7 +13,10 @@ update-alternatives --set mozilla-javaplugin.so /usr/lib/jvm/java-8-openjdk-amd6
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 apt-get update
-apt-get upgrade
 apt-get install -y google-chrome-stable
 
+# Upgrade
+apt-get upgrade
+
+# DKMS
 dpkg-reconfigure virtualbox-guest-dkms
