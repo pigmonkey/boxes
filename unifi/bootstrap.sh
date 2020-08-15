dc43ecfceb010dc72d0176337b01251a0a1b6312
Pig Monkey <pm@pig-monkey.com>
2020-08-14 18:52:10 -0700

    update unifi box to 18.04

--- unifi/bootstrap.sh
+++ unifi/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-echo "deb http://www.ubnt.com/downloads/unifi/debian stable ubiquiti" >> /etc/apt/sources.list
-apt-key adv --keyserver keyserver.ubuntu.com --recv C0A52C50
+echo "deb https://www.ui.com/downloads/unifi/debian stable ubiquiti" | tee /etc/apt/sources.list.d/100-ubnt-unifi.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv 06E85760C0A52C50
 apt-get update
 apt-get install -y unifi
 service unifi restart
